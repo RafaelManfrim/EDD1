262ec113494b5857bd15ddd23a721936f9c4a2e2
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2024-04-12 21:48:56 -0300

    Aula 12/04/2024

--- Exercícios/aula06/04_troca_referencia.cpp
+++ Exercícios/aula06/04_troca_referencia.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+void trocar_valores(float &v1, float &v2) {
+    float temp = v1;
+    v1 = v2;
+    v2 = temp;
+}
+
 int main() {
+    float v1 = 5.50, v2 = 9.99;
+    cout << "Valores iniciais:" << endl;
+    cout << "v1: " << v1 << endl;
+    cout << "v2: " << v2 << endl;
+
+    trocar_valores(v1, v2);
+
+    cout << "Valores trocados:" << endl;
+    cout << "v1: " << v1 << endl;
+    cout << "v2: " << v2 << endl;
 
     return 0;
 }
--- Exercícios/aula06/05_incremento_vetor_referencias.cpp
+++ Exercícios/aula06/05_incremento_vetor_referencias.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
+void incrementar_valores(int (&arr)[3], int tamanho) {
+    for(int i = 0; i < tamanho; i++) {
+        arr[i] += 1;
+    }
+}
+
 int main() {
+    int arr[] = {2, 4, 5};
+    cout << "Valores iniciais: " << endl;
+    cout << "arr[0]: " << arr[0] << endl;
+    cout << "arr[1]: " << arr[1] << endl;
+    cout << "arr[2]: " << arr[2] << endl;
+
+    incrementar_valores(arr, 3);
+
+    cout << "Valores incrementados: " << endl;
+
+    cout << "arr[0]: " << arr[0] << endl;
+    cout << "arr[1]: " << arr[1] << endl;
+    cout << "arr[2]: " << arr[2] << endl;
 
     return 0;
 }
