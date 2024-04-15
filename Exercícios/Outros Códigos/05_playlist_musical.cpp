3abc81963002f2a2f1d93974910dbdf2156750c9
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2024-04-15 16:47:39 -0300

    Playlist musical

--- Exercícios/Outros Códigos/05_playlist_musical.cpp
+++ Exercícios/Outros Códigos/05_playlist_musical.cpp
@@ -1,22 +1,77 @@
 #include <iostream>
+#include <cstring>
+#include <limits>
 
 using namespace std;
 
-//Crie um programa que gerencia uma playlist musical. O programa deve permitir adicionar e listar músicas na playlist.
-//Cada música deve ter um nome e uma duração em minutos.
-//Utilize uma estrutura Musica com dois campos: um ponteiro para um vetor de caracteres para o nome da música, e um inteiro para a duração.
-//Implemente as seguintes funções:
-//
-//adicionaMusica: Recebe um ponteiro para o vetor de estruturas Musica (a playlist), o nome da música, a duração, e o número atual de músicas na playlist.
-//Adiciona a nova música no final da playlist. Assume que a playlist tem capacidade para 10 músicas.
-//
-//listaMusicas: Recebe o ponteiro para o vetor de estruturas Musica e o número atual de músicas na playlist.
-//Lista todas as músicas adicionadas, mostrando o nome e a duração de cada uma.
-//
-//Para simplificar, você pode assumir que o nome da música não excederá 100 caracteres.
+struct Musica {
+    char* nome;
+    int duracao;
+};
 
-int main() {
-
-    return 0;
+void listaMusicas(Musica* playlist, int &qtde_musicas) {
+    cout << "Quantidade de músicas: " << qtde_musicas << endl;
+    for(int i = 0; i < qtde_musicas; i++) {
+        cout << "Nome da Musica: " << playlist[i].nome << endl;
+        cout << "Duracao: " << playlist[i].duracao << " minutos" << endl;
+    }
 }
 
+void adicionaMusica(Musica* playlist, int &qtde_musicas, char* nome, int duracao) {
+    Musica novaMusica;
+    novaMusica.nome = new char[strlen(nome) + 1];
+    strcpy(novaMusica.nome, nome);
+    novaMusica.duracao = duracao;
+    playlist[qtde_musicas++] = novaMusica;
+}
+
+void exibirMenu() {
+    cout << "=== Menu ===" << endl;
+    cout << "1. Adicionar Música" << endl;
+    cout << "2. Listar Músicas" << endl;
+    cout << "3. Sair" << endl;
+    cout << "Sua opção: ";
+}
+
+int main() {
+    Musica playlist[2];
+    int qtde_musicas = 0;
+    int opcao;
+
+    while(true) {
+        exibirMenu();
+        cin >> opcao;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        switch (opcao) {
+            case 1:
+                if(qtde_musicas == 2) {
+                    cout << "A playlist está cheia!" << endl;
+                    continue;
+                }
+
+                char nome[101];
+                int duracao;
+                cout << "Informe o nome da música: ";
+                cin.getline(nome, 101);
+                cout << "Informe a duração: ";
+                cin >> duracao;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                adicionaMusica(playlist, qtde_musicas, nome, duracao);
+                break;
+            case 2:
+                listaMusicas(playlist, qtde_musicas);
+                break;
+            case 3:
+                for (int i = 0; i < qtde_musicas; i++) {
+                    delete[] playlist[i].nome;
+                }
+
+                cout << "Obrigado por usar nossa playlist!";
+                return 0;
+            default:
+                cout << "Informe uma opção válida!" << endl;
+        }
+    }
+}
