456666641126cffb90f31fed4e1de1e205ab76a7
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2024-04-24 09:22:03 -0300

    Exercício G, Aquecimento Maratona 2022

---
+++ Maratonas/2022/Aquecimento/ex-G.py
@@ -0,0 +1,25 @@
+import sys
+
+numero = int(sys.argv[1])
+
+soma_pares = 0
+soma_impares = 0
+
+while(numero > 0):
+  resto = numero % 10
+  numero = numero // 10
+
+  if resto % 2 == 0:
+    soma_pares += resto
+  else:
+    soma_impares += resto
+
+if soma_pares % 3 == 0:
+  print("S")
+else:
+  print("N")
+
+if soma_impares % 3 == 0:
+  print("S")
+else:
+  print("N")
